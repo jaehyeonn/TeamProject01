0b827d93b0106e9526045ddcaf17bbc37d3cdb5e
ZEZE\a0104 <a01049469494@gmail.com>
2023-07-07 17:36:47 +0900

    feat : 모든 억까를 해치고 돌아옴

--- My project01/Assets/Scripts/PlayerHP.cs
+++ My project01/Assets/Scripts/PlayerHP.cs
@@ -25,24 +25,14 @@ public class PlayerHP : MonoBehaviour
         //현재 체력을 damage만큼 감소
         currentHP -= damage;
 
-        StopCoroutine("HitColorAnimation");
-        StartCoroutine("HitColorAnimation");
-
+        //체력이 0이하이면 플레이어 사망
         if (currentHP <= 0)
         {
             Debug.Log("Player HP : 0..Die");
         }
     }
 
-    private IEnumerator HitColorAnimation()
-    {
-        //플레이어의 색상을 빨간색으로
-        spriteRenderer.color = Color.red;
-        //0.1초 동안 대기
-        yield return new WaitForSeconds(0.1f);
-        //플레이어의 색상을 원래 색상인 하얀색으로
-        //원래 색상이 하얀색이 아닐 경우 원래 색상 변수 선언
-    }
+   
 
     //체력이 0이하 = 플레이어 캐릭터 사망
     
